3906cc81c07229e19f3a58c696c7fa5f303e0955
malcolmbly <malcolmhbailey@gmail.com>
2022-06-02 11:00:40 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.createElement('div');
+//Set container to be vertical flex
+
+function initializePage(){
+    for (let i = 0; i < 16; i++){
+        const rowContainer = document.createElement('div');
+        //TODO: Use classes to set row containers as horizontal flex
+        for (let j = 0; j < 16; j++){
+            const gridSquare = document.createElement('div');
+        }
+    }
+}
