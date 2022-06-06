320ac6429a0e217a6f331fad49c8922d800fbc72
malcolmbly <malcolmhbailey@gmail.com>
2022-06-06 10:33:19 -0600

    Changed resize to keep a fixed total grid size, instead of fixed tile sizes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const maxWidth = 1000;
-const maxHeight = 1000;
+const maxWidth = 1400;
+const maxHeight = 800;
 let currentSize = 16;
 
 const body = document.getElementsByName("body");
@@ -32,14 +32,18 @@ function createNGrid(n) {
         for (let j = 0; j < n; j++){
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('tile');
+            
+            gridSquare.style.width = Math.round(maxWidth / n) + "px";
+            gridSquare.style.height = Math.round(maxHeight / n) + "px";
+            
 
             //TODO update to make that box 10% darker each hover instead of immediatley being black
-            gridSquare.addEventListener('mouseenter', (e) => e.target.classList.add("highlighted"));
+            // add a darken method that uses (this) reference to update the style on that box.
+            gridSquare.addEventListener('mouseenter', darken);
             //gridSquare.addEventListener('mouseleave', (e) => e.target.classList.remove("highlighted"));
 
             rowContainer.appendChild(gridSquare);
         }
-        
         container.appendChild(rowContainer);
     }
 }
@@ -62,51 +66,73 @@ function resize() {
 
 function decreaseSize(n) {
 
+
+    currentSize -= n;
+    console.log(currentSize);
     for (let i = 0; i < n; i++){
         const row = container.firstElementChild;
         
         row.remove();
 
         container.childNodes.forEach(element => {
+            //for each row, remove the first tile
             element.firstChild.remove();
-        });
+        })
     }
 
-    currentSize -= n;
+    //for each row, update the remaining tiles to the new size.
+    container.childNodes.forEach(element => {
+
+        element.childNodes.forEach(tile => {
+            tile.style.width = Math.round(maxWidth / currentSize) + "px";
+            tile.style.height = Math.round(maxHeight / currentSize) + "px";
+        })
+    });
 }
 
 function increaseSize(n) {
 
+    currentSize += n;
     const currentRowsCollection = container.children;
-
-    for (let i = 0; i < currentSize; i++){
+    //update current rows
+    for (let i = 0; i < currentSize - n; i++){
 
         const row = currentRowsCollection[i];
+
+        row.childNodes.forEach(element => {
+            element.style.width = Math.round(maxWidth / currentSize) + "px";
+            element.style.height = Math.round(maxHeight / currentSize) + "px";
+        })
 
         for (let j = 0; j < n; j++){
 
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('tile');
+
+            gridSquare.style.width = Math.round(maxWidth / currentSize) + "px";
+            gridSquare.style.height = Math.round(maxHeight / currentSize) + "px";
     
             //TODO update to make that box 10% darker each hover instead of immediatley being black
-            gridSquare.addEventListener('mouseenter', (e) => e.target.classList.add("highlighted"));
+            gridSquare.addEventListener('mouseenter', darken);
             //gridSquare.addEventListener('mouseleave', (e) => e.target.classList.remove("highlighted"));
             row.appendChild(gridSquare);
         }
     }
 
+    // add new rows
     for (let i = 0; i < n; i++){
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('row');
         //TODO: Use classes to set row containers as horizontal flex
-        for (let j = 0; j < currentSize + n; j++){
+        for (let j = 0; j < currentSize; j++){
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('tile');
 
-            //TODO update to make that box 10% darker each hover instead of immediatley being black
-            gridSquare.addEventListener('mouseenter', (e) => e.target.classList.add("highlighted"));
-            //gridSquare.addEventListener('mouseleave', (e) => e.target.classList.remove("highlighted"));
+            gridSquare.style.width = Math.round(maxWidth / currentSize) + "px";
+            gridSquare.style.height = Math.round(maxHeight / currentSize) + "px";
 
+            //TODO update to make that box 10% darker each hover instead of immediatley being black
+            gridSquare.addEventListener('mouseenter', darken);
             rowContainer.appendChild(gridSquare);
         }
 
@@ -114,18 +140,26 @@ function increaseSize(n) {
         container.appendChild(rowContainer);
     }
 
-    currentSize += n;
+
 }
 
 function clear() {
 
     for (let i = 0; i < currentSize; i++){
 
-        
         container.removeChild(container.firstChild);
     }
     
     initializePage();
+}
 
+function darken(event) {
 
+    const currentTile = event.target;
+
+}
+
+function randomColor(event) {
+
+    const currentTile = event.target;
 }
